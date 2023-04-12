bec6ea4e4f58d51d7e4286fc8b938554cbeedb91
Atushi-tanaka <segawahinako@gmail.com>
2023-04-12 15:18:16 +0000

    4/12

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
   root 'static_pages#top'
+  
+  get 'posts/index'
   get '/signup', to: 'users#new'
   
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   get '/users/:user_id/users', to: 'users#index'
-  
+
   resources :users
 end
