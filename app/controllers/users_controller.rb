90c6164cadb7a7330c35bf782e0a3c97df1ce6b8
Atushi-tanaka <segawahinako@gmail.com>
2023-12-05 14:14:43 +0000

    12/5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,39 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
-
+  before_action :set_user, only: %i(show edit update destroy)
+  before_action :logged_in_user, only: %i(index show edit update destroy)
+  before_action :admin_user, only: %i(index destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_or_correct, only: %i(show)
+  
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
   end
-
+  
   def show
-    @user = User.find(params[:id])
   end
 
   def new
+    if logged_in? && !current_user.admin?
+      flash[:info] = 'すでにログインしています。'
+      redirect_to current_user
+    end
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = '新規作成に成功しました。'
+      flash[:success] = 'ユーザーの新規作成に成功しました。'
       redirect_to @user
     else
       render :new
     end
   end
-
+  
   def edit
-    @user = User.find(params[:id])
   end
-
+  
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
@@ -39,56 +42,20 @@ class UsersController < ApplicationController
       render :edit      
     end
   end
-
+  
   def destroy
     @user.destroy
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
-
+  
   private
-
+  
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-
-    # beforeフィルター
-
-    # paramsハッシュからユーザーを取得します。
+    
     def set_user
-      if params[:id] == "users"
-    # ユーザー一覧ページにアクセスした場合の処理を記述する（任意のコードを追加）
-      else
-        @user = User.find(params[:id])
-      end
-    end
-    
-    # ログイン済みのユーザーか確認します。
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-
-    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
-    def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
     end
-
-    # システム管理権限所有かどうか判定します。
-    def admin_user
-      redirect_to root_url unless current_user.admin?
-    end
-    
-    def admin_or_correct_user
-      @user = User.find(params[:user_id]) if @user.blank?
-      unless current_user?(@user) || current_user.admin?
-        flash[:danger] = "編集権限がありません。"
-        redirect_to(root_url)
-      end  
-    end
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  
-  get 'posts/index'
   get '/signup', to: 'users#new'
   
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
   
-  get '/:user_id/users', to: 'users#index'
-  get 'about', to: 'home#about'
-  get 'posts/new', to: 'posts#new'
-  post 'posts/create', to: 'posts#create'
-  get 'posts/index', to: 'posts#index'
-  get 'posts/:id', to: 'posts#show', as: :post
-  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
-
-  resources :users
-  resources :tasks, only: [:index, :new, :create]
+  resources :users do
+    resources :tasks
+  end
 end
